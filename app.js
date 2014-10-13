91eb6b372147899cc1fab1c7690744d52b5bbeb0
nucleardreamer <nucleardreamer@gmail.com>
2014-10-13 12:14:26 -0700

    added express and ping route

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('up')
+var express = require('express'),
+	app = express();
+
+var routes = require('./routes')(app);
+
+var server = app.listen(process.env.PORT || 8000, function() {
+	console.log('Listening on port %d', server.address().port);
+});
