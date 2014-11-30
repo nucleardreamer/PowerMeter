d7004b8c18e415eaa55021a86cb25beec55ccd1f
nucleardreamer <nucleardreamer@gmail.com>
2014-11-29 18:57:45 -0800

    adding put and get methods for basic node data

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ app.set('view engine', 'jade');
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+app.use(bodyParser.text());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,40 @@
 var express = require('express');
+var _ = require('lodash');
 var router = express.Router();
+var path = require('path');
+var wind = require(path.join('..','lib','wind_scrape'));
+
+var incomingResults = {};
 
 router.get('/', function(req, res) {
 	res.render('index', { title: 'Power Meter' });
 });
 
-router.get('/ping', function(req, res){
-	res.status(200).end()
+router.get('/wind', function(req, res){
+
 });
+
+router.put('/node', function(req, res){
+	var data = JSON.parse(req.body);
+	console.log(data);
+	if(_.isUndefined(incomingResults[data.nodeNumber])){
+		incomingResults[data.nodeNumber] = {
+			readings: []
+		}
+	}
+	incomingResults[data.nodeNumber].readings.push(data.reading);
+
+	res.status(200).end();
+
+});
+
+router.get('/node/readings', function(req, res){
+	res.status(200).json(incomingResults);
+});
+
+router.get('/ping', function(req, res){
+	res.status(200).end();
+});
+
 
 module.exports = router;
