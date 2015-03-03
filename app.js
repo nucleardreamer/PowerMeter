409532e22b41583ce7f93a8d6a8c72a766dd78eb
Flynn Joffray <nucleardreamer@gmail.com>
2015-03-02 21:59:22 -0800

    undo

---
+++ app.js
@@ -0,0 +1,75 @@
+process.env.TZ = 'America/Los_Angeles';
+
+require('newrelic');
+var event = require('events');
+var ee = new event.EventEmitter();
+
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
+
+var _wind = require(path.join(__dirname,'lib','wind_scrape'));
+var wind = new _wind();
+
+var routes = require('./routes/index')(ee, wind);
+
+var app = express();
+var server = require('http').Server(app);
+
+var io = require(path.join(__dirname,'lib','io'))(server, wind, ee);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+var debug = require('debug')('power-meter');
+
+server.listen(process.env.PORT || 8000, function() {
+    debug('Express server listening on port ' + (process.env.PORT || 8000));
+});
+
