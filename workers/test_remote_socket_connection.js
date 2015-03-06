02e7266217be3fd17d1813956a279e8052382835
Flynn Joffray <nucleardreamer@gmail.com>
2015-03-06 15:45:33 -0800

    updating worker

--- workers/test_remote_socket_connection.js
+++ workers/test_remote_socket_connection.js
@@ -1,29 +1,5 @@
-var url = (process.env.NODE_ENV == 'production') ? "http://www.node-hive.io" : "http://localhost:8000";
+var Reporter = require('./node_socket');
 
-var io = require('socket.io-client')(url);
-var os = require('os');
-io.on('connect', function(socket){
-    console.log('connection');
-    var reg = {
-        nodeNum: 0,
-        os: os.platform(),
-        arch: os.arch(),
-        uptime: os.uptime(),
-        mem: {
-            total: os.totalmem(),
-            free: os.freemem()
-        }
-    };
+var node_0 = new Reporter(0);
 
-    // timeout just to be safe
-    setTimeout(function(){
-        io.emit('node_register', reg);
-    }, 500);
-
-});
-io.on('event', function(data){
-    console.log('event', data);
-});
-io.on('disconnect', function(){
-    console.log('disconnect')
-});
+console.log(node_0);
