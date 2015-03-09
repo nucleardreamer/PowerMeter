d7cb5765c3e6f749a3120a5c36f253534cd078d0
nucleardreamer <nucleardreamer@gmail.com>
2015-03-08 22:12:53 -0700

    more ghetto csv dump

--- workers/export_reading_to_csv.js
+++ workers/export_reading_to_csv.js
@@ -1,4 +1,5 @@
 var fs = require('fs'),
+    _ = require('lodash'),
     path = require('path'),
     json2csv = require('json2csv'),
     _db = require(path.join(__dirname, '..','lib', 'mongo'));
@@ -9,10 +10,48 @@ var mongo = new _db(null, function(started){
 
     mongo.getAllReadings(function(err, _all){
 
+        var parsed = _.map(_all, function(item){
+            var toRet = _.clone(item._doc);
+            toRet['node_0_power'] = 0;
+            toRet.node_0_temp = 0;
+            toRet.node_1_power = 0;
+            toRet.node_1_temp = 0;
+            toRet.node_2_power = 0;
+            toRet.node_2_temp = 0;
+            toRet.node_3_power = 0;
+            toRet.node_3_temp = 0;
+
+            toRet['node_'+toRet.nodeNum+'_power'] = toRet.reading;
+            toRet['node_'+toRet.nodeNum+'_temp'] = toRet.temp;
+            return toRet;
+        });
+
         json2csv({
-            data: _all,
-            fields: ['nodeNum', 'time' ,'reading', 'temp', 'excess'],
-            fieldNames: ['Node Number', 'Time', 'Power Reading', 'Temperature', 'Wind generation was in excess']
+            data: parsed,
+            fields: [
+                'time' ,
+                'node_0_power',
+                'node_0_temp',
+                'node_1_power',
+                'node_1_temp',
+                'node_2_power',
+                'node_2_temp',
+                'node_3_power',
+                'node_3_temp',
+                'excess'
+            ],
+            fieldNames: [
+                'time',
+                'node_0_power',
+                'node_0_temp',
+                'node_1_power',
+                'node_1_temp',
+                'node_2_power',
+                'node_2_temp',
+                'node_3_power',
+                'node_3_temp',
+                'wind_gen_was_excess'
+            ]
         }, function(err, csv) {
             if (err) console.log(err);
 
